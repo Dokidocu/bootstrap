c7fbe988b625e8671595beafa9d3d03a75a03b36
Henri La <sparky008@gmail.com>
2019-03-31 21:34:55 +0200

    Install Chrome

--- osx_install.sh
+++ osx_install.sh
@@ -29,4 +29,7 @@ sudo chsh -s /usr/local/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+#chrome
+brew cask install google-chrome
+
 echo "bootstrapping finished"

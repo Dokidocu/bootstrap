7cbec6363561314a460b73507d349c90fef8e342
Henri La <sparky008@gmail.com>
2021-03-04 20:49:09 +0100

    Update script and add new softwares

--- osx_install.sh
+++ osx_install.sh
@@ -2,11 +2,11 @@
 
 # Bootstrap script for setting up a new OSX machine
 
-echo "Starting bootstrapping"
+echo "########## Starting bootstrapping"
 
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
-    echo "Installing homebrew..."
+    echo "##### Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -14,15 +14,15 @@ fi
 xcode-select —-install
 
 # Update homebrew recipes
-echo "Updating homebrew"
+echo "##### Updating homebrew"
 brew update
 
 # iterm2
-echo "Installing iterm2"
-brew cask install iterm2
+echo "##### Installing iterm2"
+brew install --cask iterm2
 
 #zsh
-echo "Installing zsh"
+echo "##### Installing zsh"
 brew install zsh
 
 sudo chsh -s /usr/local/bin/zsh
@@ -30,53 +30,67 @@ sudo chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #chrome
-echo "Installing google-chrome"
-brew cask install google-chrome
+echo "##### Installing google-chrome"
+brew install --cask google-chrome
 
 #spotify
-echo "Installing spotify"
-brew cask install spotify
+echo "##### Installing spotify"
+brew install --cask spotify
 
 #sublime-text
-echo "Installing sublime-text"
-brew cask install sublime-text
+echo "##### Installing sublime-text"
+brew install --cask sublime-text
 
 #cyberduck
-echo "Installing cyberduck"
-brew cask install cyberduck
+echo "##### Installing cyberduck"
+brew install --cask cyberduck
+
+#Charles proxy
+echo "##### Installing Charles proxy"
+brew install --cask charles
 
 #git
-echo "Installing git"
+echo "##### Installing git"
 brew install git
 
 #android studio
-echo "Installing android-studio"
-brew cask install android-studio
-brew cask install intel-haxm
+echo "##### Installing android-studio"
+brew install --cask android-studio
+brew install --cask intel-haxm
 
 #slack, microsoft-teams and skype-for-business
-echo "Installing slack, microsoft-teams and skype-for-business"
-brew cask install slack
-brew cask install microsoft-teams
-brew cask install skype-for-business
+echo "##### Installing slack, microsoft-teams and skype-for-business"
+brew install --cask slack
+brew install --cask microsoft-teams
+brew install --cask skype-for-business
 
 #docker
-echo "Installing docker"
-brew cask install docker
+echo "##### Installing docker"
+brew install --cask docker
 
 #visual-paradigm-ce
-echo "Installing visual-paradigm-ce"
+echo "##### Installing visual-paradigm-ce"
 brew install visual-paradigm-ce
 
 #gradle
-echo "Installing gradle"
-brew cask install adoptopenjdk
+echo "##### Installing gradle"
+brew install --cask adoptopenjdk
 brew install gradle
 
 #cocoapods
+echo "##### Installing cocoapods"
 sudo gem install cocoapods
 
 #sourcetree
-brew cask install sourcetree
+echo "##### Installing sourcetree"
+brew install --cask sourcetree
 
-echo "bootstrapping finished"
+#StarUML
+echo "##### Installing staruml"
+brew install --cask staruml
+
+#Omnigraffle
+echo "##### Installing omnigraffle"
+brew install --cask omnigraffle
+
+echo "##########  bootstrapping finished"
